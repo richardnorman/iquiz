e571662b156cf1c6b8c18580d0fc5038d5a937c4
Richard Norman <richard.norman.id@gmail.com>
2020-07-27 16:01:31 -0600

    Updates

--- precache-manifest.124f56088a2918a010aa69468ba71344.js
+++ precache-manifest.124f56088a2918a010aa69468ba71344.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "c984cb1f0015b4cd2a779a9072950793",
+    "revision": "1844c633ec6ac26dac292097d34b40e3",
     "url": "/iquiz/index.html"
   },
   {
-    "revision": "68e92f2cef1ca7676923",
+    "revision": "fa8998864e5b871ed733",
     "url": "/iquiz/static/css/main.9556b519.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/iquiz/static/js/2.f06589e1.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "68e92f2cef1ca7676923",
-    "url": "/iquiz/static/js/main.b1bb48fa.chunk.js"
+    "revision": "fa8998864e5b871ed733",
+    "url": "/iquiz/static/js/main.05112b3d.chunk.js"
   },
   {
     "revision": "7ae529287d3880429802",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/iquiz/precache-manifest.1ef4ad315b2de102d39c2ac575eee4e9.js"
+  "/iquiz/precache-manifest.124f56088a2918a010aa69468ba71344.js"
 );
 
 self.addEventListener('message', (event) => {
