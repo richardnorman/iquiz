67662f9b3e587288b655a918376544facf5a09e1
Richard Norman <richard.norman.id@gmail.com>
2020-07-27 15:29:47 -0600

    Updates

---
+++ precache-manifest.a98704d9e8290615d87babbda47c0bf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2be93c54422c68429916396d05b3990",
+    "url": "/iquiz/index.html"
+  },
+  {
+    "revision": "538fff1d41199925f3e1",
+    "url": "/iquiz/static/css/main.ff67c87d.chunk.css"
+  },
+  {
+    "revision": "2f11e38b3be768a537d7",
+    "url": "/iquiz/static/js/2.f06589e1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/iquiz/static/js/2.f06589e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "538fff1d41199925f3e1",
+    "url": "/iquiz/static/js/main.b1bb48fa.chunk.js"
+  },
+  {
+    "revision": "7ae529287d3880429802",
+    "url": "/iquiz/static/js/runtime-main.4d82d480.js"
+  }
+]);
